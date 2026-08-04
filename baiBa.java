3cf018962d5b11f4e622bb1e29f8078e798eaa74
Nguyễn Tâm Công <merumi148@gmail.com>
2026-08-04 11:21:32 +0700

    bai 3

---
+++ baiBa.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class baiBa {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhap n: ");
+        int n = sc.nextInt();
+
+        boolean laSoNguyenTo = true;
+
+        if (n < 2) {
+            laSoNguyenTo = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    laSoNguyenTo = false;
+                    break;
+                }
+            }
+        }
+
+        if (laSoNguyenTo) {
+            System.out.println(n + " la so nguyen to.");
+        } else {
+            System.out.println(n + " khong phai la so nguyen to.");
+        }
+
+        sc.close();
+    }
+}
