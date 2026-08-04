059a5d41e1ca11d25e4f8f4a7ca23517b4d4929f
Nguyễn Tâm Công <merumi148@gmail.com>
2026-08-04 11:40:58 +0700

    bai 4, bai 5

---
+++ baiBon.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class baiBon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhap canh a: ");
+        double a = scanner.nextDouble();
+        System.out.print("Nhap canh b: ");
+        double b = scanner.nextDouble();
+        System.out.print("Nhap canh c: ");
+        double c = scanner.nextDouble();
+
+
+        if (a > 0 && b > 0 && c > 0 && (a + b > c) && (a + c > b) && (b + c > a)) {
+            System.out.println("Ba so a, b, c Tao thanh mot tam giac.");
+
+            double eps = 1e-9;
+            boolean isVuongAtA = Math.abs(b * b + c * c - a * a) < eps;
+            boolean isVuongAtB = Math.abs(a * a + c * c - b * b) < eps;
+            boolean isVuongAtC = Math.abs(a * a + b * b - c * c) < eps;
+            boolean isVuong = isVuongAtA || isVuongAtB || isVuongAtC;
+
+            if (a == b && b == c) {
+                System.out.println("Loai tam giac: Tam giac DEU");
+            } else if (isVuong && (a == b || b == c || a == c)) {
+                System.out.println("Loai tam giac: Tam giac VUONG CAN");
+            } else if (isVuong) {
+                System.out.println("Loai tam giac: Tam giac VUONG");
+            } else if (a == b || b == c || a == c) {
+                System.out.println("Loai tam giac: Tam giac CAN");
+            } else {
+                System.out.println("Loai tam giac: Tam giac THUONG");
+            }
+        } else {
+            System.out.println("Ba so a, b, c KHONG PHAI la 3 canh cua mot tam giac.");
+        }
+
+        scanner.close();
+    }
+}
---
+++ baiNam.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class baiNam {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhap n nguyen duong: ");
+        int n = scanner.nextInt();
+
+        if (n <= 0) {
+            System.out.println("Vui long nhap so nguyên duong n > 0.");
+        } else {
+            System.out.print(n + " so Fibonacci dau tien: ");
+
+            long f0 = 1;
+            long f1 = 1;
+
+            for (int i = 1; i <= n; i++) {
+                if (i == 1) {
+                    System.out.print(f0 + " ");
+                } else if (i == 2) {
+                    System.out.print(f1 + " ");
+                } else {
+                    long fn = f0 + f1;
+                    System.out.print(fn + " ");
+                    f0 = f1;
+                    f1 = fn;
+                }
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
