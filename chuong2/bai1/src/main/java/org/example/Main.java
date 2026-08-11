77243dbfe22af9505d6672db375208a3d066e01d
Nguyễn Tâm Công <merumi148@gmail.com>
2026-08-11 10:55:43 +0700

    bai 1 chuong 2

--- chuong2/bai1/src/main/java/org/example/Main.java
+++ chuong2/bai1/src/main/java/org/example/Main.java
@@ -6,9 +6,9 @@ import javax.swing.JLabel;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("My First Swing App");
 
-        JLabel label = new JLabel("My First Swing App");
+        JLabel label = new JLabel("Hello world");
 
         frame.add(label);
         frame.setLocation(800,400);
