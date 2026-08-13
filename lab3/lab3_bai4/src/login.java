d9e16e2c6ba16d624c0b91a164bd4a088a85d22a
Nguyễn Tâm Công <merumi148@gmail.com>
2026-08-13 10:56:13 +0700

    bài 6 lab 3

--- lab3/lab3_bai4/src/login.java
+++ lab3/lab3_bai4/src/login.java
@@ -1,2 +1,41 @@
-public class login {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class login extends JFrame{
+    private JPanel panel1;
+    private JTextField tk_field;
+    private JTextField mk_field;
+    private JButton login_btn;
+    private JLabel tb;
+    private String tk = "admin";
+    private String mk = "123";
+    public login() {
+
+        setContentPane(panel1);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+        tb.setVisible(false);
+        login_btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                login();
+            }
+        });
+    }
+    void login(){
+        String acc = tk_field.getText().trim();
+        String pass = mk_field.getText().trim();
+
+        if(acc.equals(tk) && pass.equals(mk)){
+            tb.setText("Đăng nhập thành công");
+            tb.setVisible(true);
+        }
+        else {
+            tb.setText("Sai tài khoản or mk");
+            tb.setVisible(true);
+        }
+
+    }
 }
